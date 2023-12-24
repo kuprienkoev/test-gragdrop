8e4f70672b11f0188edfb7847958304e6de2cc10
Evgeny <kevik@inbox.ru>
2023-12-24 13:24:55 +0300

    LastVersionMenu
    Закончено разработка меню

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ReactComponent as Icon } from './menuIcon.svg';
 import './Header.css';
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -6,7 +7,7 @@ const Header = () => {
   return (
     <header className="header">
       <span className="header__logo">Logo1</span>
-      <nav className="header__nav">
+      <nav className={`header__nav   ${isOpen ? 'active' : ''}`}>
         <ul className="header__nav-list">
           <li className="header__nav-item">Main</li>
           <li className="header__nav-item">About</li>
@@ -15,6 +16,12 @@ const Header = () => {
           <li className="header__nav-item">Login</li>
         </ul>
       </nav>
+      <button
+        className="header__menu-button"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Icon />
+      </button>
     </header>
   );
 };
